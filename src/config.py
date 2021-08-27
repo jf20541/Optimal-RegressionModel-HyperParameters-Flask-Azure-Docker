9775f2a5e582657141b70419e5f69bc8615ae70a
jf20541 <jf20541@github.com>
2021-08-26 22:03:00 -0400

    changing config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 TRAINING_FILE = "../inputs/train.csv"
-CLEAN_FILE = "../inputs/train_clean.csv"
-TRAINING_FILE_OHE = "../inputs/train_ohe.csv"
-TRAINING_FILE_TARGET = "../inputs/train_target_encode.csv"
+TRAINING_CLEAN= "../inputs/train_clean.csv"
+TRAINING_OHE = "../inputs/train_ohe.csv"
+TRAINING_TARGET = "../inputs/train_target_encode.csv"
